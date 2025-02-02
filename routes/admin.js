d72a1a52696c187b45eb96b89c9a7cd3a5996ea4
adithyan <adithyanmkd123@gmail.com>
2025-02-02 19:10:27 +0530

    list user completed

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,21 @@
 const express = require("express");
 
+//model importing
+const User = require("../models/User.js");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("admin page");
+//list all users
+router.get("/", async (req, res) => {
+  const allUsers = await User.find();
+  console.log(allUsers);
+  res.render("admin/dashboard", { users: allUsers });
+});
+
+//delete user
+router.post("/delete", async (req, res) => {
+  console.log(req.body);
+  res.send(req.body.email);
 });
 
 module.exports = router;
